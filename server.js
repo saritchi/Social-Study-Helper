a8d1107566863d2364dafab7f7e78bdb84db937e
scott <Scott.Plummer18@gmail.com>
2020-03-11 22:16:48 -0700

    Fix spelling on error, and remove unused test functions

--- server.js
+++ server.js
@@ -16,24 +16,6 @@ database.initializeTablesIfNeeded();
 
 var port = 3003
 
-app.get('/api/serverTime', (req, res) => {
-    const serverTime = "The current time on the server is: " + Date.now();
-    console.log(serverTime);
-    res.json({result: serverTime});
-});
-
-app.get('/api/serverData', (req, res) => {
-    let r = Math.random().toString(36).substring(7);
-    console.log("random string: " + r);
-    res.json({result: r});
-});
-
-app.get('/api/cardData', (req, res) => {
-    let lp = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel mollis ante. Suspendisse nulla lorem, tempus nec congue vel, aliquam.";
-    console.log("Lorem Ipsum: " + lp);
-    res.json({result: lp});
-});
-
 //TODO: endpoint will need a query paremeter for the number of courses.
 app.get('/api/courses', (req, res) => {
     console.log("Getting courses....");
@@ -41,7 +23,7 @@ app.get('/api/courses', (req, res) => {
     database.runQuery(addCourseSQL, [], (error, results, fields) => {
         if (error) {
             console.log(`Unable to get courses from the database. Error: ${error.message}`)
-            res.status(500).json({result: "An error occured while attempting to get your courses. Please try again later."})
+            res.status(500).json({result: "An error occurred while attempting to get your courses. Please try again later."})
         } 
 
         var courses = [];
@@ -69,7 +51,7 @@ app.post('/api/addCourse', (req, res) => {
     database.runQuery(addCourseSQL, [coursename], (error) => {
         if (error) {
             console.log(`Unable to add course with name: ${coursename} to database. Error: ${error.message}`)
-            res.status(500).json({result: "An error occured while attempting to add the course to the database. Please try again later."})
+            res.status(500).json({result: "An error occurred while attempting to add the course to the database. Please try again later."})
         }
 
         console.log("Add course with name: " + coursename);
