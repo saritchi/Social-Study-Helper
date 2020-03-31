3ac441648bd36ac753296a8533ebcaf01b9efb43
scott <Scott.Plummer18@gmail.com>
2020-03-31 12:08:27 -0700

    Update code to handle deployment

--- app/app.js
+++ app/app.js
@@ -38,10 +38,11 @@ app.use('/api', require('./controllers/shared-contents'))
 //In production we will be servering our javascript and html files from a optimized build folder in client. This sets up the endpoint and exposes
 //the build folder to the browser
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client/build')));
+    const buildDir = '../client/build'
+    app.use(express.static(path.join(__dirname, buildDir)));
 
     app.get('/*', function(req, res) {
-        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, buildDir, 'index.html'));
     });
 }
 
--- client/src/components/subcomponents/InfoCard.js
+++ client/src/components/subcomponents/InfoCard.js
@@ -5,21 +5,20 @@ import OptionsDropdown from './OptionsDropdown'
 
 const InfoCard = (props) => {
     return (
-        //Render the card and if needed render the header with a vertical dropdown icon
         <Card>
-            { props.options && 
+            {props.options && 
                 <CardHeader>
                     <OptionsDropdown name={props.info} id={props.id} 
-                                     sharedWithUsers={props.sharedWithUsers}
-                                     shareContentCallback={props.shareContentCallback}
-                                     removeSharedContentCallback={props.removeSharedContentCallback}
+                                    sharedWithUsers={props.sharedWithUsers}
+                                    shareContentCallback={props.shareContentCallback}
+                                    removeSharedContentCallback={props.removeSharedContentCallback}
                     />
-                </CardHeader> }
+                </CardHeader>
+            }
             <CardBody onClick={() => props.changePage(props.id, props.info)}>
                 <p>{props.info}</p>
             </CardBody>
         </Card>
     ); 
 }
-
 export default InfoCard;
