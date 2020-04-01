a1159178c7a84864bbb1ebe40c726538b1f369cf
Aidan <aearle@sfu.ca>
2020-03-31 23:47:03 -0700

    Datetime input for tests

--- client/src/components/TestModal.js
+++ client/src/components/TestModal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Modal, ModalBody, ModalHeader } from 'shards-react'
+import { Button, Modal, ModalBody, ModalHeader, FormInput } from 'shards-react'
 import MultiSelect from "react-multi-select-component";
 
 export default class TestModal extends React.Component {
@@ -32,7 +32,6 @@ export default class TestModal extends React.Component {
             { label: "test3", value: "T3"},
             { label: "test4", value: "T4"},
         ];
-        var selected = [];
 
         var header = (isExam) ? "New Test" : "New Test for " + this.props.coursename;
         console.log(header);
@@ -44,7 +43,6 @@ export default class TestModal extends React.Component {
                     <Modal size="lg" open={open} toggle={this.toggle}>
                         <ModalHeader>{header}</ModalHeader>
                         <ModalBody>
-                            <h3>Decks</h3>
                             <pre>{JSON.stringify(this.state.selected)}</pre>
                             <MultiSelect
                                 options={options}
@@ -52,6 +50,10 @@ export default class TestModal extends React.Component {
                                 onChange={(temp) => {this.setState({selected: temp})}}
                                 labelledBy={"Select Decks for test"}>
                             </MultiSelect>
+                            <FormInput type="datetime-local" >
+                                
+
+                            </FormInput>
                         </ModalBody>
                     </Modal>
                 </>
